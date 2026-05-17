7833968fc4bb638f4b34b6530b6849ab580c09b6
liuliu6416 <987020276@qq.com>
2026-05-17 10:58:45 +0800

    update

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = 'pance-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon.svg'];
+const VERSION = '8';
+const CACHE = 'pance-v' + VERSION;
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {}))
   );
+  // 新版本立即激活
   self.skipWaiting();
 });
 
@@ -14,7 +16,14 @@ self.addEventListener('activate', (e) => {
       Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
     )
   );
+  // 接管所有页面
   self.clients.claim();
+  // 通知所有打开的页面刷新
+  self.clients.matchAll({ type: 'window' }).then((clients) => {
+    clients.forEach((client) => {
+      client.postMessage({ type: 'UPDATE_READY', version: VERSION });
+    });
+  });
 });
 
 self.addEventListener('fetch', (e) => {
@@ -22,7 +31,6 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(
       (cached) => cached || fetch(e.request).then((resp) => {
-        // Cache successful responses
         if (resp.status === 200) {
           const clone = resp.clone();
           caches.open(CACHE).then((c) => c.put(e.request, clone));
@@ -31,4 +39,9 @@ self.addEventListener('fetch', (e) => {
       }).catch(() => cached)
     )
   );
+});
+
+// 定期检查更新（每30分钟）
+self.addEventListener('message', (e) => {
+  if (e.data === 'SKIP_WAITING') self.skipWaiting();
 });
